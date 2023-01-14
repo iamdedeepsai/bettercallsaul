05f565bde484483d1cfaf0f188998d90526d64eb
gitxxyl <raghavkumar0807@gmail.com>
2023-01-15 04:06:54 +0800

    sdfjklsdkf

--- src/wolfram.js
+++
@@ -1,9 +0,0 @@
-const  appid = "H4L68G-75H7XKWLU2";
-
-function request(query){
-	query = encodeURIComponent(query)
-	console.log('?i=' + query + '&appid=' + appid);
-	fetch("https://api.wolframalpha.com/v1/spoken?i=" + query + '&appid=' + appid)
-	.then(res => res.text());
-	return text;
-}
