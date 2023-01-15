97170bfa920e49a7ad9014acb68b36d6fbdbbf71
iamdedeepsai <dedeepsai.vadapalli@gmail.com>
2023-01-15 09:50:59 +0800

    did some sauling

--- src/components/Controller/saul.js
+++ src/components/Controller/saul.js
@@ -1,19 +1,9 @@
-const FakeYou = require('fakeyou.js');
-
-const fy = new FakeYou.Client({
-    usernameOrEmail: 'saidedeepsai@gmail.com',
-    password: '^P0Q64%Z'
-});
 
 
 const getVoice = (person, text) => {
-    fy.start().then(async r => {
-        let model = fy.searchModel('person').first();
-        const result = await model.request(text);
-        const link = result.audioURL();
-        const audio = new Audio(link);
-        await audio.play().then(() => console.log("Playing sound"));
-    });
+
 }
+
+//getVoice('Saul Goodman', "We love to remove balls from the society of babies")
 
 export default getVoice;
--- src/components/Controller/wolframalpha.js
+++ src/components/Controller/wolframalpha.js
@@ -1,12 +1,28 @@
+import getVoice from "./saul";
+const FakeYou = require('fakeyou.js');
+
+const fy = new FakeYou.Client({
+    usernameOrEmail: 'saidedeepsai@gmail.com',
+    password: '^P0Q64%Z'
+});
+
+
 const appid = "H4L68G-75H7XKWLU2";
 
 function requestText(query){
     query = encodeURIComponent(query)
+
+
     fetch("https://api.wolframalpha.com/v1/spoken?i=" + query + '&appid=' + appid)
         .then(res => {
             res.text().then(r => {
                 alert(r);
-                return r;
+                fy.start().then(r => {
+                    let model = fy.searchModel('Saul Goodman').first();
+                    const result = model.request(r);
+                    const link = result.audioURL();
+                    console.log(link);
+                }).then(() => {return r});
             })
         });
 }
